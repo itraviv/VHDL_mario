e32dacd2e06541738005fbc4c2c93b48713fe4d1
holylandman <saareliad@campus.technion.ac.il>
2017-09-13 14:55:18 +0300

    Reverting commit and adding 3 more golds

--- Siftah_try/GoldBomb/Golds_mgr.vhd
+++ Siftah_try/GoldBomb/Golds_mgr.vhd
@@ -75,6 +75,12 @@ signal d_7_out : std_logic;
 signal d_8_out : std_logic;
 signal d_9_out : std_logic;
 signal d_10_out : std_logic;
+signal d_11_out : std_logic;
+signal d_12_out : std_logic;
+signal d_13_out : std_logic;
+signal d_14_out : std_logic;
+signal d_15_out : std_logic;
+signal d_16_out : std_logic;
 
 signal c_1_q : STD_LOGIC_VECTOR(8 DOWNTO 0);
 signal c_2_q : STD_LOGIC_VECTOR(8 DOWNTO 0);
@@ -86,6 +92,13 @@ signal c_7_q : STD_LOGIC_VECTOR(8 DOWNTO 0);
 signal c_8_q : STD_LOGIC_VECTOR(8 DOWNTO 0);
 signal c_9_q : STD_LOGIC_VECTOR(8 DOWNTO 0);
 signal c_10_q : STD_LOGIC_VECTOR(8 DOWNTO 0);
+signal c_11_q : STD_LOGIC_VECTOR(8 DOWNTO 0);
+signal c_12_q : STD_LOGIC_VECTOR(8 DOWNTO 0);
+signal c_13_q : STD_LOGIC_VECTOR(8 DOWNTO 0);
+signal c_14_q : STD_LOGIC_VECTOR(8 DOWNTO 0);
+signal c_15_q : STD_LOGIC_VECTOR(8 DOWNTO 0);
+signal c_16_q : STD_LOGIC_VECTOR(8 DOWNTO 0);
+
 
 signal b_1_drawing_request : std_logic;
 signal b_1_hit : std_logic;
@@ -106,6 +119,20 @@ signal b_4_mVGA_RGB : STD_LOGIC_VECTOR(7 DOWNTO 0);
 signal b_5_drawing_request : std_logic;
 signal b_5_hit : std_logic;
 signal b_5_mVGA_RGB : STD_LOGIC_VECTOR(7 DOWNTO 0);
+
+signal b_6_drawing_request : std_logic;
+signal b_6_hit : std_logic;
+signal b_6_mVGA_RGB : STD_LOGIC_VECTOR(7 DOWNTO 0);
+
+signal b_7_drawing_request : std_logic;
+signal b_7_hit : std_logic;
+signal b_7_mVGA_RGB : STD_LOGIC_VECTOR(7 DOWNTO 0);
+
+signal b_8_drawing_request : std_logic;
+signal b_8_hit : std_logic;
+signal b_8_mVGA_RGB : STD_LOGIC_VECTOR(7 DOWNTO 0);
+
+
 
 -----------------
 --clk dividers --
@@ -178,6 +205,47 @@ GENERIC MAP(Div => 777
 PORT MAP(Clk_in => CLK,
 		 Resetn => RESETn,
 		 Clk_out => d_10_out);
+		 
+d_11 : clk_divider
+GENERIC MAP(Div => 111
+			)
+PORT MAP(Clk_in => CLK,
+		 Resetn => RESETn,
+		 Clk_out => d_11_out);
+
+
+d_12 : clk_divider
+GENERIC MAP(Div => 242
+			)
+PORT MAP(Clk_in => CLK,
+		 Resetn => RESETn,
+		 Clk_out => d_12_out);
+
+d_13 : clk_divider
+GENERIC MAP(Div => 571
+			)
+PORT MAP(Clk_in => CLK,
+		 Resetn => RESETn,
+		 Clk_out => d_13_out);
+d_14 : clk_divider
+GENERIC MAP(Div => 175
+			)
+PORT MAP(Clk_in => CLK,
+		 Resetn => RESETn,
+		 Clk_out => d_14_out);
+
+d_15 : clk_divider
+GENERIC MAP(Div => 971
+			)
+PORT MAP(Clk_in => CLK,
+		 Resetn => RESETn,
+		 Clk_out => d_15_out);
+		 
+d_16 : clk_divider
+GENERIC MAP(Div => 71			)
+PORT MAP(Clk_in => CLK,
+		 Resetn => RESETn,
+		 Clk_out => d_16_out);
 
 
 -------------------
@@ -194,6 +262,12 @@ c_7 : lpm_counter_cyclic PORT MAP(clock => d_7_out , q=> c_7_q );
 c_8 : lpm_counter_cyclic PORT MAP(clock => d_8_out , q=> c_8_q );		 
 c_9 : lpm_counter_cyclic PORT MAP(clock => d_9_out , q=> c_9_q );		 
 c_10 : lpm_counter_cyclic PORT MAP(clock => d_10_out , q=> c_10_q );		 
+c_11 : lpm_counter_cyclic PORT MAP(clock => d_11_out , q=> c_11_q );		 
+c_12 : lpm_counter_cyclic PORT MAP(clock => d_12_out , q=> c_12_q );		 
+c_13 : lpm_counter_cyclic PORT MAP(clock => d_13_out , q=> c_13_q );		 
+c_14 : lpm_counter_cyclic PORT MAP(clock => d_14_out , q=> c_14_q );		 
+c_15 : lpm_counter_cyclic PORT MAP(clock => d_15_out , q=> c_15_q );		 
+c_16 : lpm_counter_cyclic PORT MAP(clock => d_16_out , q=> c_16_q );		 
 
 -----------------
 --golds fulls-- --
@@ -281,7 +355,54 @@ PORT MAP(CLK => CLK,
 		 drawing_request => b_5_drawing_request,
 		 hit => b_5_hit,
 		 mVGA_RGB => b_5_mVGA_RGB);
-
+		 
+b_6_F : goldfull
+PORT MAP(CLK => CLK,
+		 RESETn => RESETn,
+		 timer_done => timer_done,
+		 allowed_to_move => '1',
+		 enable => '1',
+		 oCoord_X => oCoord_X,
+		 oCoord_Y => oCoord_Y,
+		 Player_X => player_X,
+		 Player_Y => player_Y,
+		 Random1 => c_11_q,
+		 Random2 => c_12_q,
+		 drawing_request => b_6_drawing_request,
+		 hit => b_6_hit,
+		 mVGA_RGB => b_6_mVGA_RGB);
+		 
+b_7_F : goldfull
+PORT MAP(CLK => CLK,
+		 RESETn => RESETn,
+		 timer_done => timer_done,
+		 allowed_to_move => '1',
+		 enable => '1',
+		 oCoord_X => oCoord_X,
+		 oCoord_Y => oCoord_Y,
+		 Player_X => player_X,
+		 Player_Y => player_Y,
+		 Random1 => c_13_q,
+		 Random2 => c_14_q,
+		 drawing_request => b_7_drawing_request,
+		 hit => b_7_hit,
+		 mVGA_RGB => b_7_mVGA_RGB);
+b_8_F : goldfull
+PORT MAP(CLK => CLK,
+		 RESETn => RESETn,
+		 timer_done => timer_done,
+		 allowed_to_move => '1',
+		 enable => '1',
+		 oCoord_X => oCoord_X,
+		 oCoord_Y => oCoord_Y,
+		 Player_X => player_X,
+		 Player_Y => player_Y,
+		 Random1 => c_15_q,
+		 Random2 => c_16_q,
+		 drawing_request => b_8_drawing_request,
+		 hit => b_8_hit,
+		 mVGA_RGB => b_8_mVGA_RGB);
+		 
 		 
 		 
 --b_6_F : goldfull
@@ -326,6 +447,9 @@ drawing_request <= '1' when b_1_drawing_request = '1' else
 			'1' when b_3_drawing_request = '1' else
 			'1' when b_4_drawing_request = '1' else
 			'1' when b_5_drawing_request = '1' else
+			'1' when b_6_drawing_request = '1' else
+			'1' when b_7_drawing_request = '1' else
+			'1' when b_8_drawing_request = '1' else
 			'0';
 			
 
@@ -336,7 +460,10 @@ mVGA_RGB   <= b_1_mVGA_RGB when b_1_drawing_request = '1' else
 			     b_2_mVGA_RGB when b_2_drawing_request = '1' else
 			     b_3_mVGA_RGB when b_3_drawing_request = '1' else
 				 b_4_mVGA_RGB when b_4_drawing_request = '1' else
-			     b_5_mVGA_RGB;
+				 b_5_mVGA_RGB when b_5_drawing_request = '1' else
+				 b_6_mVGA_RGB when b_6_drawing_request = '1' else
+				 b_7_mVGA_RGB when b_7_drawing_request = '1' else
+			     b_8_mVGA_RGB;
 				 
 ---------------
 --    Hits   --
@@ -346,7 +473,9 @@ hit <= '1' when b_1_hit = '1' else
 			'1' when b_3_hit = '1' else
 			'1' when b_4_hit = '1' else
 			'1' when b_5_hit = '1' else
+			'1' when b_6_hit = '1' else
+			'1' when b_7_hit = '1' else
+			'1' when b_8_hit = '1' else
 			'0';
-
 
 end architecture;
